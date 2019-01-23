6811e21e0cc03233f7c21d7b99f7e1162ef61215
Rhys Tyers <rhystyers1@gmail.com>
2019-01-23 11:26:44 +0000

    use filename at end of url rather than search params

--- dist/photoswipe.js
+++ dist/photoswipe.js
@@ -1,4 +1,4 @@
-/*! PhotoSwipe - v4.1.3 - 2019-01-08
+/*! PhotoSwipe - v4.1.3 - 2019-01-23
 * http://photoswipe.com
 * Copyright (c) 2019 Dmitry Semenov; */
 (function (root, factory) { 
@@ -3463,12 +3463,12 @@ var _historyDefaultOptions = {
 };
 
 var _historyUpdateTimeout,
-	_hashChangeTimeout,
-	_hashAnimCheckTimeout,
-	_hashChangedByScript,
-	_hashChangedByHistory,
-	_hashReseted,
-	_initialHash,
+	_fileChangeTimeout,
+	_fileAnimCheckTimeout,
+	_fileChangedByScript,
+	_fileChangedByHistory,
+	_fileNameReseted,
+	_initialFileName,
 	_historyChanged,
 	_closedFromURL,
 	_urlChangedOnce,
@@ -3476,133 +3476,120 @@ var _historyUpdateTimeout,
 
 	_supportsPushState,
 
-	_getHash = function() {
-		return _windowLoc.hash.substring(1);
+	_isFile = function () {
+		var filename = _windowLoc.pathname.substring(_windowLoc.pathname.lastIndexOf('/') + 1);
+		return filename.includes('.')
 	},
-	_cleanHistoryTimeouts = function() {
+	_getFileName = function () {
+		var filename = _windowLoc.pathname.substring(_windowLoc.pathname.lastIndexOf('/') + 1);
+		return filename.includes('.') ? filename : '';
+	},
+	_getDirName = function () {
+		var filename = _windowLoc.pathname.substring(_windowLoc.pathname.lastIndexOf('/') + 1);
+		return filename.includes('.') ? _windowLoc.pathname.substring(0, _windowLoc.pathname.lastIndexOf('/')) : _windowLoc.pathname;
+	},
+	_cleanHistoryTimeouts = function () {
 
-		if(_historyUpdateTimeout) {
+		if (_historyUpdateTimeout) {
 			clearTimeout(_historyUpdateTimeout);
 		}
 
-		if(_hashAnimCheckTimeout) {
-			clearTimeout(_hashAnimCheckTimeout);
+		if (_fileAnimCheckTimeout) {
+			clearTimeout(_fileAnimCheckTimeout);
 		}
 	},
 
 	// pid - Picture index
-	// gid - Gallery index
-	_parseItemIndexFromURL = function() {
-		var hash = _getHash(),
-			params = {};
-
-		if(hash.length < 5) { // pid=1
+	_parseItemIndexFromURL = function () {
+		var filename = _getFileName();
+		var params = {};
+		if (!filename) {
 			return params;
 		}
-
-		var i, vars = hash.split('&');
-		for (i = 0; i < vars.length; i++) {
-			if(!vars[i]) {
-				continue;
-			}
-			var pair = vars[i].split('=');	
-			if(pair.length < 2) {
-				continue;
-			}
-			params[pair[0]] = pair[1];
-		}
-		if(_options.galleryPIDs) {
+		if (_options.galleryPIDs) {
 			// detect custom pid in hash and search for it among the items collection
-			var searchfor = params.pid;
+			var searchfor = filename;
 			params.pid = 0; // if custom pid cannot be found, fallback to the first item
-			for(i = 0; i < _items.length; i++) {
-				if(_items[i].pid === searchfor) {
+			for (i = 0; i < _items.length; i++) {
+				if (_items[i].pid === searchfor) {
 					params.pid = i;
 					break;
 				}
 			}
-		} else {
-			params.pid = parseInt(params.pid,10)-1;
 		}
-		if( params.pid < 0 ) {
+		if (params.pid < 0) {
 			params.pid = 0;
 		}
 		return params;
 	},
-	_updateHash = function() {
+	_updateURL = function () {
 
-		if(_hashAnimCheckTimeout) {
-			clearTimeout(_hashAnimCheckTimeout);
+		if (_fileAnimCheckTimeout) {
+			clearTimeout(_fileAnimCheckTimeout);
 		}
 
 
-		if(_numAnimations || _isDragging) {
+		if (_numAnimations || _isDragging) {
 			// changing browser URL forces layout/paint in some browsers, which causes noticable lag during animation
 			// that's why we update hash only when no animations running
-			_hashAnimCheckTimeout = setTimeout(_updateHash, 500);
+			_fileAnimCheckTimeout = setTimeout(_updateURL, 500);
 			return;
 		}
-		
-		if(_hashChangedByScript) {
-			clearTimeout(_hashChangeTimeout);
+
+		if (_fileChangedByScript) {
+			clearTimeout(_fileChangeTimeout);
 		} else {
-			_hashChangedByScript = true;
+			_fileChangedByScript = true;
 		}
 
 
 		var pid = (_currentItemIndex + 1);
-		var item = _getItemAt( _currentItemIndex );
-		if(item.hasOwnProperty('pid')) {
+		var item = _getItemAt(_currentItemIndex);
+		if (item.hasOwnProperty('pid')) {
 			// carry forward any custom pid assigned to the item
 			pid = item.pid;
 		}
-		var newHash = _initialHash + '&'  +  'gid=' + _options.galleryUID + '&' + 'pid=' + pid;
+		var newFile = _getDirName() + '/' + pid;
 
-		if(!_historyChanged) {
-			if(_windowLoc.hash.indexOf(newHash) === -1) {
+		if (!_historyChanged) {
+			if (_windowLoc.pathname.indexOf(newFile) === -1) {
 				_urlChangedOnce = true;
 			}
 			// first time - add new hisory record, then just replace
 		}
 
-		var newURL = _windowLoc.href.split('#')[0] + '#' +  newHash;
+		var newURL = _windowLoc.protocol + '//' + _windowLoc.host + '/' + newFile.replace(/^\/+|\/+$/g, '');;
 
-		if( _supportsPushState ) {
-
-			if('#' + newHash !== window.location.hash) {
-				history[_historyChanged ? 'replaceState' : 'pushState']('', document.title, newURL);
-			}
-
+		if (_supportsPushState) {
+			history[_historyChanged ? 'replaceState' : 'pushState']('', document.title, newURL);
 		} else {
-			if(_historyChanged) {
-				_windowLoc.replace( newURL );
-			} else {
-				_windowLoc.hash = newHash;
+			if (_historyChanged) {
+				_windowLoc.replace(newURL);
 			}
 		}
-		
-		
+
+
 
 		_historyChanged = true;
-		_hashChangeTimeout = setTimeout(function() {
-			_hashChangedByScript = false;
+		_fileChangeTimeout = setTimeout(function () {
+			_fileChangedByScript = false;
 		}, 60);
 	};
 
 
 
-	
+
 
 _registerModule('History', {
 
-	
+
 
 	publicMethods: {
-		initHistory: function() {
+		initHistory: function () {
 
 			framework.extend(_options, _historyDefaultOptions, true);
 
-			if( !_options.history ) {
+			if (!_options.history) {
 				return;
 			}
 
@@ -3611,119 +3598,93 @@ _registerModule('History', {
 			_urlChangedOnce = false;
 			_closedFromURL = false;
 			_historyChanged = false;
-			_initialHash = _getHash();
+			_initialFileName = _getFileName();
 			_supportsPushState = ('pushState' in history);
 
 
-			if(_initialHash.indexOf('gid=') > -1) {
-				_initialHash = _initialHash.split('&gid=')[0];
-				_initialHash = _initialHash.split('?gid=')[0];
-			}
-			
-
 			_listen('afterChange', self.updateURL);
-			_listen('unbindEvents', function() {
-				framework.unbind(window, 'hashchange', self.onHashChange);
+			_listen('unbindEvents', function () {
+				framework.unbind(window, 'popstate', self.onFileChange);
 			});
 
 
-			var returnToOriginal = function() {
-				_hashReseted = true;
-				if(!_closedFromURL) {
+			var returnToOriginal = function () {
+				_fileNameReseted = true;
+				if (!_closedFromURL) {
 
-					if(_urlChangedOnce) {
+					if (_urlChangedOnce) {
 						history.back();
 					} else {
 
-						if(_initialHash) {
-							_windowLoc.hash = _initialHash;
-						} else {
-							if (_supportsPushState) {
-
-								// remove hash from url without refreshing it or scrolling to top
-								history.pushState('', document.title,  _windowLoc.pathname + _windowLoc.search );
-							} else {
-								_windowLoc.hash = '';
-							}
+						if (_supportsPushState) {
+							history.pushState('', document.title, _getDirName());
 						}
 					}
-					
+
 				}
 
 				_cleanHistoryTimeouts();
 			};
 
 
-			_listen('unbindEvents', function() {
-				if(_closedByScroll) {
+			_listen('unbindEvents', function () {
+				if (_closedByScroll) {
 					// if PhotoSwipe is closed by scroll, we go "back" before the closing animation starts
 					// this is done to keep the scroll position
 					returnToOriginal();
 				}
 			});
-			_listen('destroy', function() {
-				if(!_hashReseted) {
+			_listen('destroy', function () {
+				if (!_fileNameReseted) {
 					returnToOriginal();
 				}
 			});
-			_listen('firstUpdate', function() {
+			_listen('firstUpdate', function () {
 				_currentItemIndex = _parseItemIndexFromURL().pid;
 			});
 
-			
-
-			
-			var index = _initialHash.indexOf('pid=');
-			if(index > -1) {
-				_initialHash = _initialHash.substring(0, index);
-				if(_initialHash.slice(-1) === '&') {
-					_initialHash = _initialHash.slice(0, -1);
-				}
-			}
-			
-
-			setTimeout(function() {
-				if(_isOpen) { // hasn't destroyed yet
-					framework.bind(window, 'hashchange', self.onHashChange);
+			setTimeout(function () {
+				if (_isOpen) { // hasn't destroyed yet
+					framework.bind(window, 'popstate', self.onFileChange);
 				}
 			}, 40);
-			
-		},
-		onHashChange: function() {
 
-			if(_getHash() === _initialHash) {
+		},
+		onFileChange: function () {
+
+			if (_getFileName() === _initialFileName) {
 
 				_closedFromURL = true;
 				self.close();
 				return;
 			}
-			if(!_hashChangedByScript) {
+			if (!_fileChangedByScript) {
 
-				_hashChangedByHistory = true;
-				self.goTo( _parseItemIndexFromURL().pid );
-				_hashChangedByHistory = false;
+				_fileChangedByHistory = true;
+				self.goTo(_parseItemIndexFromURL().pid);
+				_fileChangedByHistory = false;
 			}
-			
+
 		},
-		updateURL: function() {
+		updateURL: function () {
 
 			// Delay the update of URL, to avoid lag during transition, 
 			// and to not to trigger actions like "refresh page sound" or "blinking favicon" to often
-			
-			_cleanHistoryTimeouts();
-			
 
-			if(_hashChangedByHistory) {
+			_cleanHistoryTimeouts();
+
+
+			if (_fileChangedByHistory) {
 				return;
 			}
 
-			if(!_historyChanged) {
-				_updateHash(); // first time
+			if (!_historyChanged) {
+				_updateURL(); // first time
 			} else {
-				_historyUpdateTimeout = setTimeout(_updateHash, 800);
+				_historyUpdateTimeout = setTimeout(_updateURL, 800);
 			}
 		}
-	
+
 	}
 });
 
